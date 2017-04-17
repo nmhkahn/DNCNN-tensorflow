1f4e20e04d023b1df2e9c30928b282ca6ebd0698
NamHyuk Ahn <nmhkahn@gmail.com>
2017-04-17 13:06:24 +0900

    Modify scripts

--- script/train.sh
+++ script/train.sh
@@ -1,9 +1,9 @@
 python dncnn/train.py \
     --image_size=96 \
     --batch_size=32 \
-    --checkpoint_basename=dncnn \
-    --learning_rate=0.001 \
-    --decay_steps=25000 \
+    --checkpoint_basename=base \
+    --learning_rate=0.002 \
+    --decay_steps=15000 \
     --decay_ratio=0.5 \
-    --max_steps=100000 \
-    --logdir=log/
+    --max_steps=30000 \
+    --logdir=log_base/
