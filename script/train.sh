4295d02a8a25073b8eaa4c8efd25ff87fc886486
Namhyuk Ahn <nmhkahn@gmail.com>
2017-05-05 14:27:41 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -1,10 +1,10 @@
 python dncnn/train.py \
-    --model=residual \
-    --checkpoint_basename=residual \
-    --logdir=log_residual \
-    --quality=10 \
+    --dataset_dir=dataset \
+    --model=base_skip \
+    --checkpoint_basename=dncnn \
+    --logdir=log_dncnn \
     --image_size=96 \
-    --batch_size=32 \
+    --batch_size=16 \
     --learning_rate=0.002 \
-    --max_steps=40000 \
+    --max_steps=100000 \
     --save_model_steps=5000 \
